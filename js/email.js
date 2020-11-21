6e6acec2c71a4195676729381eeaf1d0e11949bf
MichalMoudry <moudry.michal@live.com>
2020-11-21 16:42:03 +0100

    Changes to email view UI

--- js/email.js
+++ js/email.js
@@ -3,7 +3,6 @@ var email = { id: "", title: "", date: "", content: "", to: "", from: "" };
 $(document).ready(function () {
     if (window.location.search.substr(1) != "") {
         var textArea = document.getElementById("email-content");
-        textArea.style.height = textArea.scrollHeight + "px";
         var localstorageKeys = Object.keys(localStorage);
         var emails = localstorageKeys.filter(element => element.includes("sent_") || element.includes("draft_") || element.includes("scheduled_"));
         email["id"] = window.location.search.substr(1).split("=")[1];
@@ -12,9 +11,16 @@ $(document).ready(function () {
         document.title = email["title"] + " - Email Sender";
         document.getElementById("email-title").textContent = email["title"];
         var emailAsJson = JSON.parse(localStorage.getItem(emails.filter(element => element.includes(email["date"]))));
+        email["date"] = emailAsJson[5];
         document.getElementById("email-from").textContent = emailAsJson[2];
         document.getElementById("email-to").textContent = emailAsJson[1];
+        document.getElementById("date-span").textContent = new Date(emailAsJson[5]).toLocaleString();
         textArea.textContent = emailAsJson[4];
-        console.log(emailAsJson);
+        textArea.style.height = textArea.scrollHeight + "px";
     }
 });
+
+function discardEmail() {
+    localStorage.removeItem("sent_" + email["title"] + email["date"]);
+    window.location.href = "./";
+}
